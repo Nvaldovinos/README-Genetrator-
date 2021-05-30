83c6935f34864c7272bf73826ce3b4a406f05fd2
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-05-30 18:58:13 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -4,12 +4,63 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    
+    
     {
         type: 'input',
-        message: 'What is your projects name? ',
-        name: 'name '
+        message: 'What is the title of your application? ',
+        name: 'name',
+    },
+    
+    {
+        type: 'input',
+        message: "Please enter a desciption of your application: ",
+        name: 'description',
+    },
+    
+    {
+        type: 'editor',
+        message: 'Explain how to install your application: ',
+        name: 'install',
+    },
 
-    }
+    {
+        type: 'input',
+        message: 'Enter usage inforamtion: ', 
+        name: 'usage',
+    },
+    
+    {   
+        type: 'input',
+        message: 'How can other contribute to this apllication? ',
+        name:  'contribute',
+    },
+
+    {   
+        type: 'input',
+        message: 'How can your application be tested? ',
+        name:  'test',
+    },
+
+    {   
+        type: 'list',
+        message: 'What type of license are you using? ',
+        name:  'license',
+        choices: ['MIT', 'Apache', 'GPLv3'],
+    },
+
+    {
+        type: 'input',
+        message: 'What is your GitHub username? ',
+        name: 'github',
+    
+    },
+    
+    {
+        type: 'input',
+        message: 'Enter your email adress: ',
+        name: 'email',
+    },
 ]
 
 // TODO: Create a function to write README file
