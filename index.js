816225a3aece0de071d48c6496e1a49c7d2da325
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-05-30 22:26:12 -0400

    updated files

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
-// TODO: Include packages needed for this application
+// Applications needed 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { fetchAsyncQuestionPropertyQuestionProperty } = require('inquirer/lib/utils/utils');
+// const path = require('./REA');
 
-// TODO: Create an array of questions for user input
-const questions = [
-    
-    
-    {
-        type: 'input',
-        message: 'What is the title of your application? ',
-        name: 'name',
-    },
-    
-    {
-        type: 'input',
-        message: "Please enter a desciption of your application: ",
-        name: 'description',
-    },
-    
-    {
-        type: 'editor',
-        message: 'Explain how to install your application: ',
-        name: 'install',
-    },
+//Array of questions for user
+function questions() {
+    inquirer.prompt([
+{
+    type: 'input',
+    message: 'What is the title of your application? ',
+    name: 'name',
+},
 
-    {
-        type: 'input',
-        message: 'Enter usage inforamtion: ', 
-        name: 'usage',
-    },
-    
-    {   
-        type: 'input',
-        message: 'How can other contribute to this apllication? ',
-        name:  'contribute',
-    },
+{
+    type: 'input',
+    message: "Please enter a desciption of your application: ",
+    name: 'description',
+},
 
-    {   
-        type: 'input',
-        message: 'How can your application be tested? ',
-        name:  'test',
-    },
+{
+    type: 'editor',
+    message: 'Explain how to install your application: ',
+    name: 'install',
+},
 
-    {   
-        type: 'list',
-        message: 'What type of license are you using? ',
-        name:  'license',
-        choices: ['MIT', 'Apache', 'GPLv3'],
-    },
+{
+    type: 'input',
+    message: 'Enter usage inforamtion: ', 
+    name: 'usage',
+},
 
-    {
-        type: 'input',
-        message: 'What is your GitHub username? ',
-        name: 'github',
+{   
+    type: 'input',
+    message: 'How can other contribute to this apllication? ',
+    name:  'contribute',
+},
+
+{   
+    type: 'input',
+    message: 'How can your application be tested? ',
+    name:  'test',
+},
+
+{   
+    type: 'list',
+    message: 'What type of license are you using? ',
+    name:  'license',
+    choices: ['MIT', 'Apache', 'GPLv3'],
+},
+
+{
+    type: 'input',
+    message: 'What is your GitHub username? ',
+    name: 'github',
+
+},
+
+{
+    type: 'input',
+    message: 'Enter your email adress: ',
+    name: 'email',
+},
     
-    },
-    
-    {
-        type: 'input',
-        message: 'Enter your email adress: ',
-        name: 'email',
-    },
-]
+])}  
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
